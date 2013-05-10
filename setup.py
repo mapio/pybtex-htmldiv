4b99125be75e09ad3d831d89bff93a524b5f6d6b
Massimo Santini <santini@di.unimi.it>
2013-05-10 15:03:48 +0200

    Added version number

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='Pybtex htmldiv',
     author='Massimo Santini <santini@di.unimi.it>',
     py_modules=['htmldiv'],
+    version="0.1",
     entry_points = {
         'pybtex.backends': 'htmldiv = htmldiv:Backend',
     },
